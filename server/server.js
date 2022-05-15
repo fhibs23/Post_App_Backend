82b4a1c256bb29aedf6d6c27c869ccd0340590ba
fhibs23 <56549850+fhibs23@users.noreply.github.com>
2022-05-15 22:42:01 +0300

    Fixing sending attachments

--- server/server.js
+++ server/server.js
@@ -4,17 +4,7 @@ const emailConfig = require("./app/config/email.config");
 const mg = require('mailgun-js');
 const multer = require("multer");
 const app = express();
-const Storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, "./attachments");
-    },
-    filename: function (req, file, callback) {
-        callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-    },
-});
-const attachmentUpload = multer({
-    storage: Storage,
-}).single("attachment");
+
 
 const mailgun = () =>
     mg({
@@ -39,7 +29,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 app.post('/api/email', (req, res) => {
-    const { from, sender, email, subject, message, attachmentPath } = req.body;
+    const { from, sender, email, subject, message } = req.body;
     mailgun()
         .messages()
         .send(
@@ -48,7 +38,6 @@ app.post('/api/email', (req, res) => {
                 to: `${email}`,
                 subject: `${subject}`,
                 html: `<p>${message}</p>`,
-                attachment: `${attachmentPath}` ,
 
             },
             (error, body) => {
